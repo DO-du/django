856895dc257c12ab954a726b9cc5dc18cb719431
ldh <daaa0555@naver.com>
2021-06-09 10:51:27 +0900

    2-02-2

--- config/settings.py
+++ config/settings.py
@@ -27,10 +27,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+ 
 # Application definition
 
 INSTALLED_APPS = [
+    'pybo.apps.PyboConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
