d4baf86d6d0bd5473d7772fdf0dce8be4a632a7f
ldh <daaa0555@naver.com>
2021-06-09 16:17:35 +0900

    2-06

--- pybo/views.py
+++ pybo/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404,redirect
 
 from .models import Question
+from django.utils import timezone
+
 
 def index(request):
     """
@@ -25,6 +27,11 @@ def detail(request, question_id):
 
     return render(request, 'pybo/question_detail.html', context)
 
-
-
+def answer_create(request, question_id):
+    """
+    pybo 답변등록
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    return redirect('pybo:detail', question_id=question.id)
 # Create your views here.
