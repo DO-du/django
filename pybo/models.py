489a6562298bf9f5d6f12a3090a228dc9430ab34
ldh <daaa0555@naver.com>
2021-06-11 17:05:56 +0900

    3-12

--- pybo/models.py
+++ pybo/models.py
@@ -9,6 +9,7 @@ class Question(models.Model):
     content=models.TextField()
     create_date=models.DateTimeField()
     modify_date = models.DateTimeField(null=True, blank=True)
+    voter = models.ManyToManyField(User, related_name='voter_question')
 
     def __str__(self):
         return self.subject
@@ -20,6 +21,7 @@ class Answer(models.Model):
     content = models.TextField()
     create_date = models.DateTimeField()
     modify_date = models.DateTimeField(null=True, blank=True)
+    voter = models.ManyToManyField(User, related_name='voter_answer')
 
  
 class Comment(models.Model):
